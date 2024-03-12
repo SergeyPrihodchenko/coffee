b78e50179e555dbcd6c3a55a12c624a881a7dd8b
Katherine <strix1112@gmail.com>
2024-03-12 10:23:53 +0300

    created MainPageController

---
+++ app/Http/Controllers/MainPage/MainPageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\MainPage;
+
+use App\Http\Controllers\Controller;
+use Illuminate\View\View;
+
+class MainPageController extends Controller
+{
+    public function index(): View
+    {
+      return view('MainPage.main');
+    }
+}
