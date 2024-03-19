8bb752a117b8a15034106d72de6fad7246ff61b1
Sergo <S.prihodchenko@yandex.ru>
2024-03-19 16:23:35 +0300

    a

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,23 +16,29 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
-const imgs = [];
+const imgPreviou = document.querySelector('.img-previou');
+imgPreviou.innerHTML = '<svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" /></svg>';
 
-const file = document.querySelector('.file-input');
-const txt = document.querySelector('.text-message');
-const sendBtn = document.querySelector('.send-btn');
+const fileInput = document.querySelector('.file-input');
 
-sendBtn.addEventListener('click', async () => {
-    axios.post('admin/product/setCoffee', {'title': txt.value})
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.log(err);
-    })
-});
+fileInput.addEventListener('change', (e) => {
+    let f = e.target.files[0];
+    if(!f.type.match('image/*')) {
+        f = [];
+    }
+    const reader = new FileReader();
+    
+    
 
-file.addEventListener('change', (e) => {
-    imgs.push(e.target.files[0]);
-});
+    reader.onloadend = () => {
+        const img = document.createElement('img');
+        img.src = reader.result;
+        img.width = 190;
+        img.height = 190;
+        img.alt = 'not loding IMG';
+        imgPreviou.removeChild(imgPreviou.lastChild);
+        imgPreviou.append(img);
+    }
 
+    reader.readAsDataURL(f);
+})
