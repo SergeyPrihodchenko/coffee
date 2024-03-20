eb722fa05d0d1a01d82bd89052591f7cb16572b4
Sergo <S.prihodchenko@yandex.ru>
2024-03-20 14:06:07 +0300

    a

--- app/Http/Controllers/admin/ProductsController.php
+++ app/Http/Controllers/admin/ProductsController.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\admin\RequestProduct;
+use App\Models\Coffee;
 
 class ProductsController extends Controller
 {
-    public function setCoffee(RequestProduct $request): void
+    public function setCoffee(RequestProduct $request):void
     {
-        try {
-            $validated = $request->validated();
-            dd($validated);
-        } catch (\Throwable $th) {
-            //throw $th;
-        }
+        $validated = $request->validated();
+
+        $path = $validated['img']->store('public/coffee');
+
+        $path = str_replace('public', '/storage', $path);   
+
+        $validated['img'] = $path;
+
+        Coffee::create($validated);
     }
 
     public function deleteCoffee(RequestProduct $request): void
@@ -28,11 +32,15 @@ class ProductsController extends Controller
 
     public function setSweet(RequestProduct $request): void
     {
-        try {
-            $validated = $request->validated();
-        } catch (\Throwable $th) {
-            //throw $th;
-        }
+        $validated = $request->validated();
+
+        $path = $validated['img']->store('public/sweet');
+
+        $path = str_replace('public', '/storage', $path);   
+
+        $validated['img'] = $path;
+
+        Coffee::create($validated);
     }
 
     public function deleteSweet(RequestProduct $request): void
