28b7ec546d29c90958eae6ed79a3f28a53d7f273
Sergey <s.prihodchenko@yandex.ru>
2024-05-23 22:33:05 +0300

    a

--- resources/views/components/messager.blade.php
+++ resources/views/components/messager.blade.php
@@ -1,5 +1,5 @@
 <div class="container_messager animate__animated animate__slideInUp">
     <div class="d-btn btn-messager">
-        <span>messager</span>
+        <span><i class="bi bi-chat-left-fill i-messager"></i></span>
       </div>
 </div>
