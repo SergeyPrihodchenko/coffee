fbfb3f1398c326bbbe8e66815018b1b0916d095d
Sergey <s.prihodchenko@yandex.ru>
2024-05-24 16:14:48 +0300

    a

--- resources/views/components/modal-messager.blade.php
+++ resources/views/components/modal-messager.blade.php
@@ -1,12 +1,18 @@
 <div class="container-modal-messager">
     <div class="modal-messager">
-        <div class="header-modal">тут чет написанно</div>
+        <div class="header-modal">
+            <p class="admin-status">online<span class="status-indicator"></span></p>
+            <div class="status-message">message for user</div>
+        </div>
         <div class="display-modal">
             <ul class="display-messages">
-                <li class="row-message f-right">test</li>
-                <li class="row-message f-left">test</li>
+                <li class="row-message f-left">мне кофе с чаем</li>
+                <li class="row-message f-right">завтра</li>
             </ul>
         </div>
-        <div class="form-modal"><input value="input" type="text" name="" id=""><button>send</button></div>
+        <div class="form-modal">
+            <button>img</button>
+            <input value="input" type="text" name="" id="" class="input-messager">
+            <button>send</button></div>
     </div>
 </div>
